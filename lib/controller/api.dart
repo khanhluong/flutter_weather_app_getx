f0af153cbf11c117bb11bab9d14e89df98d41519
khanh.luong <baokhanh2710cntt@gmail.com>
2021-02-27 14:14:09 +1300

    init proj

---
+++ lib/controller/api.dart
@@ -0,0 +1,1 @@
+final String apiKey = "89155897fe4479216968af61f3f223a6";
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weather_app_getx/controller/bindings/weather_bind.dart';
+import 'package:flutter_weather_app_getx/ui/weather_screen.dart';
+import 'package:get/get.dart';
+
+void main() {
+  WeatherBind().dependencies();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      initialRoute: '/',
+      getPages: [GetPage(name: '/', page: () => WeatherScreen())],
+    );
+  }
+}
